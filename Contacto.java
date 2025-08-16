82544260962fb19771118b26a1b0ebb38be04c9e
Marcelo193231314 <marcelo.silva.am06@gmail.com>
2025-08-16 17:25:43 -0600

    Add files via upload

---
+++ Contacto.java
@@ -0,0 +1,16 @@
+
+public class Contacto {
+    String nombre;
+    String direccion;
+    String telefono;
+
+    public Contacto(String nombre, String direccion, String telefono) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public String toString() {
+        return nombre + " - " + direccion + " - " + telefono;
+    }
+}
---
+++ Node.java
@@ -0,0 +1,12 @@
+
+class Node {
+    Object data;
+    Node next;
+    Node prev;
+
+    public Node(Object data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
